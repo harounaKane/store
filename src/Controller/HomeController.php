6eefe8f40cd862acd88fb18678cfc6a3184901c3
harounaKane <harouna.Kane@hotmail.fr>
2024-05-29 17:11:22 +0200

    creation entity et CRUD

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,58 +2,22 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController{
 
-    private $articles;
-
-    public function __construct(){
-        $this->articles = [
-            [
-                "id" => 1,
-                "libelle" => "PC",
-                "prix" => 780
-            ],
-            [
-                "id" => 2,
-                "libelle" => "Clavier",
-                "prix" => 80
-            ],
-            [
-                "id" => 3,
-                "libelle" => "Ecran",
-                "prix" => 180
-            ]
-        ];
-    }
-
 
     #[Route('/', name: 'app_home')]
-    public function index(): Response{
+    public function index(ArticleRepository $articleRepo): Response{
+
 
         return $this->render('home/index.html.twig', [
-            "articles" => $this->articles,
+            "articles" => $articleRepo->findAll(),
             "title" => "Accueil"
         ]);
     }
 
-    #[Route("/article/show/{id}", name: "app_home_show")]
-    public function show($id){
-        $art = $this->articles[$id-1];
-        
-        return $this->render("home/show.html.twig", ["article" => $art]);
-    }
-
-    #[Route("/article/somme/prix", name: "app_home_somme")]
-    public function somme(){
-        $somme = 0;
-        foreach($this->articles as $article)
-            $somme += $article["prix"];
-
-        
-        return $this->render("home/somme.html.twig", ["somme" => $somme]);
-    }
 }
---
+++ src/Entity/Article.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ArticleRepository::class)]
+class Article
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 30)]
+    private ?string $libelle = null;
+
+    #[ORM\Column]
+    private ?float $prix = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $create_at = null;
+
+    #[ORM\Column]
+    private ?int $stock = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLibelle(): ?string
+    {
+        return $this->libelle;
+    }
+
+    public function setLibelle(string $libelle): static
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    public function getPrix(): ?float
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(float $prix): static
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getCreateAt(): ?\DateTimeImmutable
+    {
+        return $this->create_at;
+    }
+
+    public function setCreateAt(\DateTimeImmutable $create_at): static
+    {
+        $this->create_at = $create_at;
+
+        return $this;
+    }
+
+    public function getStock(): ?int
+    {
+        return $this->stock;
+    }
+
+    public function setStock(int $stock): static
+    {
+        $this->stock = $stock;
+
+        return $this;
+    }
+}
